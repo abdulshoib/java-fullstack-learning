f479e3af2611ab5bbe91de6bbca18559ed46db08
sal-maan <abdulsalmaan9581@gmail.com>
2026-03-18 15:11:53 +0530

    Day 2: Even and Odd Sum programs

---
+++ OddSum.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class OddSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {   // check odd
+                sum = sum + i;
+            }
+        }
+
+        System.out.println("Sum of Odd numbers: " + sum);
+    }
+}
