e09aa1bfca3557343c41e283511f523e119b288a
abdulshoib <abdulshoib6281@gmail.coml>
2026-04-02 23:21:16 +0530

    Day 3: String programs (reverse, palindrome, vowels)

---
+++ Day03/ReverseString.java
@@ -0,0 +1,16 @@
+package Day03;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String str = "java";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        System.out.println("Reversed = " + rev);
+    }
+}
