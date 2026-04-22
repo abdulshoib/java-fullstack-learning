0920f3f80c03d321813e309e20996579079af89a
Shoib <abdulshoib6281@gmail.com>
2026-04-22 20:00:33 +0530

    BankConnection.java

---
+++ Week4/BankConnection.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class BankConnection {
+
+    public static void main(String[] args) {
+
+        try {
+            // 1. Load connection
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/bankdb",
+                "root",
+                "your_password" // change this
+            );
+
+            // 2. Check connection
+            if (con != null) {
+                System.out.println("Database Connected Successfully 🚀");
+            }
+
+            // 3. Close connection
+            con.close();
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
